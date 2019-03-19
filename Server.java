fba6972d5fd52b12c5134a4c8ee949c46540bf33
therbtltrmz <48274605+therbtltrmz@users.noreply.github.com>
2019-03-19 22:47:04 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -20,7 +20,7 @@ public class Server
             System.out.println("Waiting for the client to connect..BE PATIENT!^^"); 
   
             socket = server.accept(); 
-            System.out.println("Client has received your request!Yayyy!!"); 
+            System.out.println("Client has received your message!Yayyy!!"); 
   
             // takes input from the client socket 
             in = new DataInputStream( 
